8c7575ec7046b5e1fd43e131d606f747653e9855
shadillac <shadmorris@gmail.com>
2014-12-29 12:35:49 -0800

    send toast

---
+++ SendToast/SendToast.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SendToast.aspx.cs" Inherits="SendToast.SendToast" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <br />
+        Enter URI:</div>
+    <asp:TextBox ID="TextBoxUri" runat="server" Width="666px"></asp:TextBox>
+    <br />
+    <br />
+    Enter Title:<br />
+    <asp:TextBox ID="TextBoxTitle" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    Enter Subtitle:<br />
+    <asp:TextBox ID="TextBoxSubTitle" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    <br />
+    <asp:Button ID="ButtonSendToast" runat="server" onclick="ButtonSendToast_Click" 
+        Text="Send Toast Notification" />
+    <br />
+    <br />
+    Response:<br />
+    <asp:TextBox ID="TextBoxResponse" runat="server" Height="78px" Width="199px"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ SendToast/SendToast.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace SendToast
+{
+    public partial class SendToast : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void ButtonSendToast_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the URI that the Microsoft Push Notification Service returns to the push client when creating a notification channel.
+                // Normally, a web service would listen for URIs coming from the web client and maintain a list of URIs to send
+                // notifications out to.
+                string subscriptionUri = TextBoxUri.Text.ToString();
+
+
+                HttpWebRequest sendNotificationRequest = (HttpWebRequest)WebRequest.Create(subscriptionUri);
+
+                // Create an HTTPWebRequest that posts the toast notification to the Microsoft Push Notification Service.
+                // HTTP POST is the only method allowed to send the notification.
+                sendNotificationRequest.Method = "POST";
+
+                // The optional custom header X-MessageID uniquely identifies a notification message. 
+                // If it is present, the same value is returned in the notification response. It must be a string that contains a UUID.
+                // sendNotificationRequest.Headers.Add("X-MessageID", "<UUID>");
+
+                // Create the toast message.
+                string toastMessage = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                "<wp:Notification xmlns:wp=\"WPNotification\">" +
+                   "<wp:Toast>" +
+                        "<wp:Text1>" + TextBoxTitle.Text.ToString() + "</wp:Text1>" +
+                        "<wp:Text2>" + TextBoxSubTitle.Text.ToString() + "</wp:Text2>" +
+                        "<wp:Param>/Page2.xaml?NavigatedFrom=Toast Notification</wp:Param>" +
+                   "</wp:Toast> " +
+                "</wp:Notification>";
+
+                // Set the notification payload to send.
+                byte[] notificationMessage = Encoding.Default.GetBytes(toastMessage);
+
+                // Set the web request content length.
+                sendNotificationRequest.ContentLength = notificationMessage.Length;
+                sendNotificationRequest.ContentType = "text/xml";
+                sendNotificationRequest.Headers.Add("X-WindowsPhone-Target", "toast");
+                sendNotificationRequest.Headers.Add("X-NotificationClass", "2");
+
+
+                using (Stream requestStream = sendNotificationRequest.GetRequestStream())
+                {
+                    requestStream.Write(notificationMessage, 0, notificationMessage.Length);
+                }
+
+                // Send the notification and get the response.
+                HttpWebResponse response = (HttpWebResponse)sendNotificationRequest.GetResponse();
+                string notificationStatus = response.Headers["X-NotificationStatus"];
+                string notificationChannelStatus = response.Headers["X-SubscriptionStatus"];
+                string deviceConnectionStatus = response.Headers["X-DeviceConnectionStatus"];
+
+                // Display the response from the Microsoft Push Notification Service.  
+                // Normally, error handling code would be here. In the real world, because data connections are not always available,
+                // notifications may need to be throttled back if the device cannot be reached.
+                TextBoxResponse.Text = notificationStatus + " | " + deviceConnectionStatus + " | " + notificationChannelStatus;
+            }
+            catch (Exception ex)
+            {
+                TextBoxResponse.Text = "Exception caught sending update: " + ex.ToString();
+            }
+
+        }
+
+    }
+}
